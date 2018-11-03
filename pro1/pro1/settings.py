370aa839cb914fa142531d9c12d7aa1b72c60b71
Monsij Biswal <biswalmonsij@gmail.com>
2018-11-04 02:31:14 +0530

    Add email id

--- pro1/pro1/settings.py
+++ pro1/pro1/settings.py
@@ -141,5 +141,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'monsijbiswal051998@gmail.com'
-EMAIL_HOST_PASSWORD = 'moon19981961'
+EMAIL_HOST_USER = 'nitdgpportal@gmail.com'
+EMAIL_HOST_PASSWORD = 'qwer1234kl'
