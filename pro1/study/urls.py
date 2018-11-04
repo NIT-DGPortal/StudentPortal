db3ff2cc3c8322249ac8eb9f82da560be7fa7a62
Monsij Biswal <biswalmonsij@gmail.com>
2018-11-04 13:54:32 +0530

    Improve study zone

--- pro1/study/urls.py
+++ pro1/study/urls.py
@@ -3,13 +3,47 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='study-home'),
+    path('year_1/year_1_doc/',views.year_1_doc, name='first-year-doc'),
+    path('year_1/',views.year_1, name='first-year'),
+
+    path('cs/year_2/',views.cs_second, name='study-cs-second'),
+    path('cs/year_3/',views.cs_third, name='study-cs-third'),
+    path('cs/year_4/',views.cs_fourth, name='study-cs-fourth'),
     path('cs/',views.cs, name='study-cs'),
+
+    path('ec/year_2/',views.ec_second, name='study-ec-second'),
+    path('ec/year_3/',views.ec_third, name='study-ec-third'),
+    path('ec/year_4/',views.ec_fourth, name='study-ec-fourth'),
     path('ec/',views.ec, name='study-ec'),
+
+    path('ee/year_2/',views.ee_second, name='study-ee-second'),
+    path('ee/year_3/',views.ee_third, name='study-ee-third'),
+    path('ee/year_4/',views.ee_fourth, name='study-ee-fourth'),
     path('ee/',views.ee, name='study-ee'),
-    path('mm/',views.mm, name='study-mm'),
-    path('bt/',views.bt, name='study-bt'),
+
+    path('it/year_2/',views.it_second, name='study-it-second'),
+    path('it/year_3/',views.it_third, name='study-it-third'),
+    path('it/year_4/',views.it_fourth, name='study-it-fourth'),
     path('it/',views.it, name='study-it'),
+
+    path('bt/year_2/',views.bt_second, name='study-bt-second'),
+    path('bt/year_3/',views.bt_third, name='study-bt-third'),
+    path('bt/year_4/',views.bt_fourth, name='study-bt-fourth'),
+    path('bt/',views.bt, name='study-bt'),
+
+    path('mm/year_2/',views.mm_second, name='study-mm-second'),
+    path('mm/year_3/',views.mm_third, name='study-mm-third'),
+    path('mm/year_4/',views.mm_fourth, name='study-mm-fourth'),
+    path('mm/',views.mm, name='study-bt'),
+
+    path('ch/year_2/',views.ch_second, name='study-ch-second'),
+    path('ch/year_3/',views.ch_third, name='study-ch-third'),
+    path('ch/year_4/',views.ch_fourth, name='study-ch-fourth'),
     path('ch/',views.ch, name='study-ch'),
+
+    path('ce/year_2/',views.ce_second, name='study-ce-second'),
+    path('ce/year_3/',views.ce_third, name='study-ce-third'),
+    path('ce/year_4/',views.ce_fourth, name='study-ce-fourth'),
     path('ce/',views.ce, name='study-ce'),
 
 ]
--- pro1/study/views.py
+++ pro1/study/views.py
@@ -10,33 +10,161 @@ def home(request):
     #return HttpResponse('<p> Hello </p>')
 
 @login_required(redirect_field_name='')
+def year_1_doc(request):
+	return render(request,'study/year_1/year_1_doc.html')
+
+@login_required(redirect_field_name='')
+def year_1(request):
+	return render(request,'study/year_1/year_1.html')
+
+
+
+
+@login_required(redirect_field_name='')
+def cs_second(request):
+	return render(request,'study/cs/cs_second.html')
+
+@login_required(redirect_field_name='')
+def cs_third(request):
+	return render(request,'study/cs/cs_third.html')
+
+@login_required(redirect_field_name='')
+def cs_fourth(request):
+	return render(request,'study/cs/cs_fourth.html')
+
+@login_required(redirect_field_name='')
 def cs(request):
-	return render(request,'study/cs.html')
+	return render(request,'study/cs/cs.html')
+
+
+
+
+@login_required(redirect_field_name='')
+def ec_second(request):
+	return render(request,'study/ec/ec_second.html')
+
+@login_required(redirect_field_name='')
+def ec_third(request):
+	return render(request,'study/ec/ec_third.html')
+
+@login_required(redirect_field_name='')
+def ec_fourth(request):
+	return render(request,'study/ec/ec_fourth.html')
 
 @login_required(redirect_field_name='')
 def ec(request):
-	return render(request,'study/ec.html')
+	return render(request,'study/ec/ec.html')
+
+
+
+
+@login_required(redirect_field_name='')
+def ee_second(request):
+	return render(request,'study/ee/ee_second.html')
+
+@login_required(redirect_field_name='')
+def ee_third(request):
+	return render(request,'study/ee/ee_third.html')
+
+@login_required(redirect_field_name='')
+def ee_fourth(request):
+	return render(request,'study/ee/ee_fourth.html')
 
 @login_required(redirect_field_name='')
 def ee(request):
-	return render(request,'study/ee.html')
+	return render(request,'study/ee/ee.html')
+
+
+
+
+@login_required(redirect_field_name='')
+def mm_second(request):
+	return render(request,'study/mm/mm_second.html')
+
+@login_required(redirect_field_name='')
+def mm_third(request):
+	return render(request,'study/mm/mm_third.html')
+
+@login_required(redirect_field_name='')
+def mm_fourth(request):
+	return render(request,'study/mm/mm_fourth.html')
 
 @login_required(redirect_field_name='')
 def mm(request):
-	return render(request,'study/mm.html')
+	return render(request,'study/mm/mm.html')
+
+
+
+
+@login_required(redirect_field_name='')
+def bt_second(request):
+	return render(request,'study/bt/bt_second.html')
+
+@login_required(redirect_field_name='')
+def bt_third(request):
+	return render(request,'study/bt/bt_third.html')
+
+@login_required(redirect_field_name='')
+def bt_fourth(request):
+	return render(request,'study/bt/bt_fourth.html')
 
 @login_required(redirect_field_name='')
 def bt(request):
-	return render(request,'study/bt.html')
+	return render(request,'study/bt/bt.html')
+
+
+
+
+@login_required(redirect_field_name='')
+def it_second(request):
+	return render(request,'study/it/it_second.html')
+
+@login_required(redirect_field_name='')
+def it_third(request):
+	return render(request,'study/it/it_third.html')
+
+@login_required(redirect_field_name='')
+def it_fourth(request):
+	return render(request,'study/it/it_fourth.html')
 
 @login_required(redirect_field_name='')
 def it(request):
-	return render(request,'study/it.html')
+	return render(request,'study/it/it.html')
+
+
+
+
+@login_required(redirect_field_name='')
+def ce_second(request):
+	return render(request,'study/ce/ce_second.html')
+
+@login_required(redirect_field_name='')
+def ce_third(request):
+	return render(request,'study/ce/ce_third.html')
+
+@login_required(redirect_field_name='')
+def ce_fourth(request):
+	return render(request,'study/ce/ce_fourth.html')
 
 @login_required(redirect_field_name='')
 def ce(request):
-	return render(request,'study/ce.html')
+	return render(request,'study/ce/ce.html')
+
+
+
+
+@login_required(redirect_field_name='')
+def ch_second(request):
+	return render(request,'study/ch/ch_second.html')
+
+@login_required(redirect_field_name='')
+def ch_third(request):
+	return render(request,'study/ch/ch_third.html')
+
+@login_required(redirect_field_name='')
+def ch_fourth(request):
+	return render(request,'study/ch/ch_fourth.html')
 
 @login_required(redirect_field_name='')
 def ch(request):
-	return render(request,'study/ch.html')
+	return render(request,'study/ch/ch.html')
