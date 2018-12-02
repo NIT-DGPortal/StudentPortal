370261076bcf8ebd3228469b367d8ca7446ba824
gopavasanth <gopavasanth1999@gmail.com>
2018-12-02 18:19:48 +0530

    Changed the Email_USE_TLS to true
    This enabes to send the emails to users for registration and password reset.

--- pro1/pro1/settings.py
+++ pro1/pro1/settings.py
@@ -139,7 +139,7 @@ LOGOUT_URL = 'logout'
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
