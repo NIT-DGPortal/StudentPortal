98f107810bfabce8b279e0fb7c06d368af6b43cf
Monsij Biswal <biswalmonsij@gmail.com>
2018-12-08 22:39:42 +0530

    Correct location

--- tests.py
+++ tests.py
@@ -1,11 +1,11 @@
 import unittest
 
-import awesome
+import pro1
 
 
 class TestMethods(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(awesome.smile(), ":)")
+        self.assertEqual(pro1.smile(), ":)")
 
 
 if __name__ == '__main__':
